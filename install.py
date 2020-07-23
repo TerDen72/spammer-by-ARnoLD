d9ccabe95a77fc06d22143a6180eb54d425bfc43
palkamilka <68155972+palkamilka@users.noreply.github.com>
2020-07-23 21:47:01 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+os.system("pkg install dos2unix")
+os.system("pip install colorama")
+os.system("pip install requests")
+os.system("cp ~/spammer-by-ARnoLD/spammer.py /data/data/com.termux/files/usr/bin/spammer-by-ARnoLD")
+os.system("dos2unix /data/data/com.termux/files/usr/bin/spammer")
+os.system("chmod -R 777 ~/spammer")
+os.system("chmod 777 /data/data/com.termux/files/usr/bin/spammer")
+os.system("spammer")
